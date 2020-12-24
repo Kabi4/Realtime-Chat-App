c813515f603a5501be1a369c9d97bb1e369e41c5
Kabi4 <hykukku@gmail.com>
2020-12-24 09:02:42 +0530

    Warming up sockets

---
+++ public/js/chat.js
@@ -0,0 +1,9 @@
+const socket = io();
+// socket.on('updatedCount', (count) => {
+//     console.log('We Have a new notification! Count:' + count);
+// });
+
+// document.querySelector('#push').addEventListener('click', () => {
+//     console.log('This one is sent by you!');
+//     socket.emit('notify');
+// });
--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 require('dotenv').config({ path: __dirname + '/.env' });
-
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 const path = require('path');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+let users = 0;
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../public'))); //setting the static path to look for at last when no where link to be found
+
+io.on('connection', (socket) => {
+    console.log('New user in websocket connected!');
+    socket.emit('updatedCount', users);
+    socket.on('notify', () => {
+        users++;
+        // socket.emit('updatedCount', users);
+        io.emit('updatedCount', users);
+    });
+});
 
 app.get('/', (req, res) => {
     res.send('index.html');
 });
 
-module.exports = app;
+module.exports = server;
